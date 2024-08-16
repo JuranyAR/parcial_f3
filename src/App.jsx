6f0357205013772f8f97004ecec0c99853cdea4b
Jurany Ramirez <juranyr9@gmail.com>
2024-08-15 20:09:30 -0500

    result exam 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,62 @@ import Card from "./components/Card";
 import './App.css'
 
 function App() {
+  const [data, setData] = useState({name:'', book:''})
+  const [error, setError] = useState('')
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (e) =>{
+    setSubmitted(false);
+    setData({...data, [e.target.name]: e.target.value})
+  }
+
+  const validate = () =>{
+    if (data.name.trim().length < 3 || data.book.trim().length < 6) {
+      return 'Por favor chequea que la información sea correcta';
+    }
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const error = validate()
+    if(!error){
+      setSubmitted(true)
+      setError('')
+    } else {
+      setError(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+    <div className="container">
+      <h1 className='title'>Cuál es tu libro favorito?</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Nombre:</label>
+          <input
+            type="text"
+            name="name"
+            placeholder='Ingresa tu nombre'
+            value={data?.name}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div>
+          <label htmlFor="title">Título del libro:</label>
+          <input
+            type="text"
+            placeholder='Ingresa tu libro favorito'
+            name="book"
+            value={data?.book}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <button type="submit">Enviar</button>
+        {error && <p className='error'>{error}</p>}
+      </form>
+      {submitted && (
+        <Card data={data} />
+      )}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+const Card = ({data}) => {
+  return (
+    <div className="card">
+      <h2>Hola! {data.name}</h2>
+      <p>Sabemos que tu libro favorito es: <b>{data.book}</b></p>
+    </div>
+  );
 };
 
 
